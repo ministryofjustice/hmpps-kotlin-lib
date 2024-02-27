6569b4326dfba39901a67234c504c6edd04cea3b
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-02-27 11:04:10 +0000

    SDIT-1336 Fix ordering of autoconfiguration

--- hmpps-kotlin-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/kotlin/auth/HmppsWebClientConfiguration.kt
+++ hmpps-kotlin-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/kotlin/auth/HmppsWebClientConfiguration.kt
@@ -1,10 +1,13 @@
 package uk.gov.justice.hmpps.kotlin.auth
 
+import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.REACTIVE
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.SERVLET
+import org.springframework.boot.autoconfigure.security.oauth2.client.reactive.ReactiveOAuth2ClientAutoConfiguration
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
@@ -28,6 +31,7 @@ import kotlin.apply as kotlinApply
 private const val DEFAULT_TIMEOUT_SECONDS: Long = 30
 private const val DEFAULT_HEALTH_TIMEOUT_SECONDS: Long = 2
 
+@AutoConfigureAfter(OAuth2ClientAutoConfiguration::class)
 @ConditionalOnWebApplication(type = SERVLET)
 @ConditionalOnBean(ClientRegistrationRepository::class)
 @Configuration
@@ -46,6 +50,7 @@ class HmppsWebClientConfiguration {
   }
 }
 
+@AutoConfigureAfter(ReactiveOAuth2ClientAutoConfiguration::class)
 @ConditionalOnWebApplication(type = REACTIVE)
 @ConditionalOnBean(ReactiveClientRegistrationRepository::class)
 @Configuration
@@ -53,12 +58,12 @@ class HmppsReactiveWebClientConfiguration {
 
   @ConditionalOnMissingBean
   @Bean
-  fun reactiveAuthorizedClientManager(
-    clientRegistrationRepository: ReactiveClientRegistrationRepository,
-    oAuth2AuthorizedClientService: ReactiveOAuth2AuthorizedClientService,
+  fun reactiveOAuth2AuthorizedClientManager(
+    reactiveClientRegistrationRepository: ReactiveClientRegistrationRepository,
+    reactiveOAuth2AuthorizedClientService: ReactiveOAuth2AuthorizedClientService,
   ): ReactiveOAuth2AuthorizedClientManager = AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
-    clientRegistrationRepository,
-    oAuth2AuthorizedClientService,
+    reactiveClientRegistrationRepository,
+    reactiveOAuth2AuthorizedClientService,
   ).kotlinApply { setAuthorizedClientProvider(ReactiveOAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()) }
 }
 
