b25dece328d112c8875c1ef1afcfc8db6005f736
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-03-11 11:56:41 +0000

    INC-1355 Add subject access request reactive services and tests (#28)
    * INC-1355 Add subject access request reactive services and tests
    
    The changes introduced new reactive services for subject access requests, catering to both prisoner reference numbers and case reference numbers. This included adding relevant tests, controllers, and service implementations with additional specifications on handling requests based on different roles and permissions. The build version was also updated to reflect these enhancements.
    
    * INC-1355: ✨ Add missing auto configuration import
    
    * INC-1355: ✨ Also add extra permission for testing
    
    ---------
    
    Co-authored-by: Peter Phillips <5099053+petergphillips@users.noreply.github.com>

---
+++ test-app-reactive/src/test/kotlin/uk/gov/justice/digital/hmpps/testappreactive/integration/service/SubjectAccessRequestReactiveServiceSampleTest.kt
@@ -0,0 +1,25 @@
+package uk.gov.justice.digital.hmpps.testappreactive.integration.service
+
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.testappreactive.service.SubjectAccessRequestService
+
+class SubjectAccessRequestReactiveServiceSampleTest {
+  private val service = SubjectAccessRequestService()
+
+  @Test
+  fun `returns null for not found`() = runTest {
+    // service will return not found for prisoners that don't start with A
+    assertThat(service.getPrisonContentFor("B12345", null, null)).isNull()
+  }
+
+  @Test
+  fun `returns data if prisoner found`() = runTest {
+    // service will return data for prisoners that start with A
+    val restrictedPatient = service.getPrisonContentFor("A12345", null, null)?.content
+
+    assertThat(restrictedPatient).extracting("prisonerNumber").isEqualTo("A12345")
+    assertThat(restrictedPatient).extracting("commentText").isEqualTo("some useful comment")
+  }
+}
