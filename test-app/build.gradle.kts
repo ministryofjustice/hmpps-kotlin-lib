398b12501fd723d7fa9e0004f51f94f4762a6f6e
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-04-19 13:32:55 +0100

    SDIT-1677: 🔒️ Upgrade to latest spring boot etc.

--- test-app/build.gradle.kts
+++ test-app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
   kotlin("plugin.spring") version "1.9.23"
 }
 
@@ -11,7 +11,7 @@ dependencies {
   implementation(project(":hmpps-kotlin-spring-boot-starter"))
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  testImplementation("org.wiremock:wiremock-standalone:3.5.2")
+  testImplementation("org.wiremock:wiremock-standalone:3.5.3")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
 }
