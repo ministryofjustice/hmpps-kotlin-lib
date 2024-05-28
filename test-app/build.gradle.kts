c607f1b2328c3aa1c6666b8aec8d56743b966642
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-05-28 17:01:29 +0100

    SDIT-1788: ⬆️ Upgrade to latest spring boot etc.

--- test-app/build.gradle.kts
+++ test-app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
-  kotlin("plugin.spring") version "1.9.23"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.0"
+  kotlin("plugin.spring") version "2.0.0"
 }
 
 configurations {
@@ -11,7 +11,7 @@ dependencies {
   implementation(project(":hmpps-kotlin-spring-boot-starter"))
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  testImplementation("org.wiremock:wiremock-standalone:3.5.3")
+  testImplementation("org.wiremock:wiremock-standalone:3.6.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
 }
@@ -22,8 +22,6 @@ kotlin {
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-      jvmTarget = "21"
-    }
+    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
   }
 }
