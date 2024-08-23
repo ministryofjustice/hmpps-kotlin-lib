cd0d778c6c2ce647eb99421eb43337376d2c594d
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-08-23 07:30:05 +0100

    SDIT-1988: ⬆️ Upgrade to hmpps plugin 6.0.5, spring boot 3.3.3 and kotlin 2.0.20 (#49)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-  kotlin("jvm") version "2.0.10" apply false
+  kotlin("jvm") version "2.0.20" apply false
 }
 
 /*
@@ -11,7 +11,7 @@ plugins {
 
 allprojects {
   group = "uk.gov.justice.service.hmpps"
-  version = "1.0.4"
+  version = "1.0.5"
 
   repositories {
     mavenCentral()
