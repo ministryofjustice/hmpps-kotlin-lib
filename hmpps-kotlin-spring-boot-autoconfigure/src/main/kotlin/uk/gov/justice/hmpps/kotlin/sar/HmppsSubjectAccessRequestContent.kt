f17ab6c37179880b399de3d954d1a5879adc2d34
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-07-24 17:34:34 +0100

    SDIT-2967: ♻️  Fix compilation warnings

--- hmpps-kotlin-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/kotlin/sar/HmppsSubjectAccessRequestContent.kt
+++ hmpps-kotlin-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/kotlin/sar/HmppsSubjectAccessRequestContent.kt
@@ -3,6 +3,6 @@ package uk.gov.justice.hmpps.kotlin.sar
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class HmppsSubjectAccessRequestContent(
-  @Schema(description = "The content of the subject access request response")
+  @param:Schema(description = "The content of the subject access request response")
   val content: Any,
 )
