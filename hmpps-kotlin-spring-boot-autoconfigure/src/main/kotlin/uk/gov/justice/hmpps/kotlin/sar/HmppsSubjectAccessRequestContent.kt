3382147152be20283a959024dd38f337006ecc9b
Inderjit Virdi <inderjit.virdi@digital.justice.gov.uk>
2025-09-09 10:47:55 +0100

    HAAR-4277: Allow for attachments to be included in SAR response

--- hmpps-kotlin-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/kotlin/sar/HmppsSubjectAccessRequestContent.kt
+++ hmpps-kotlin-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/kotlin/sar/HmppsSubjectAccessRequestContent.kt
@@ -5,4 +5,14 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class HmppsSubjectAccessRequestContent(
   @param:Schema(description = "The content of the subject access request response")
   val content: Any,
+  val attachments: List<Attachment>? = null,
+)
+
+data class Attachment(
+  val attachmentNumber: Int,
+  val name: String,
+  val contentType: String,
+  val url: String,
+  val filesize: Int,
+  val filename: String,
 )
