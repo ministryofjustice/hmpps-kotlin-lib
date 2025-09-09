bd6ad4e40a583faa353237280dafeda86f88ef11
indyv-moj <inderjit.virdi@justice.gov.uk>
2025-09-09 13:51:05 +0100

    HAAR-4277: Allow for attachments to be included in SAR response (#93)
    * HAAR-4277: Allow for attachments to be included in SAR response
    
    * HAAR-4277: Add openapi documentation to SAR attachment fields and increment lib version
    
    * HAAR-4277: Fix SAR api reponse openapi documentation
    
    * Update build.gradle.kts
    
    Co-authored-by: petergphillips <5099053+petergphillips@users.noreply.github.com>
    
    * Update release-notes/1.x.md
    
    Co-authored-by: petergphillips <5099053+petergphillips@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: petergphillips <5099053+petergphillips@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 allprojects {
   group = "uk.gov.justice.service.hmpps"
-  version = "1.6.0"
+  version = "1.7.0-beta"
 
   repositories {
     mavenCentral()
--- hmpps-kotlin-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/kotlin/sar/HmppsSubjectAccessRequestContent.kt
+++ hmpps-kotlin-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/kotlin/sar/HmppsSubjectAccessRequestContent.kt
@@ -3,6 +3,23 @@ package uk.gov.justice.hmpps.kotlin.sar
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class HmppsSubjectAccessRequestContent(
-  @param:Schema(description = "The content of the subject access request response")
+  @field:Schema(description = "The content of the subject access request response")
   val content: Any,
+  @field:Schema(description = "The details of any attachments for the subject access request response")
+  val attachments: List<Attachment>? = null,
+)
+
+data class Attachment(
+  @field:Schema(description = "The number of the attachment which will match any corresponding reference in the content section")
+  val attachmentNumber: Int,
+  @field:Schema(description = "The name or description of the attachment which will be included in the report")
+  val name: String,
+  @field:Schema(description = "The content type of the attachment")
+  val contentType: String,
+  @field:Schema(description = "The url to be used to download the attachment file")
+  val url: String,
+  @field:Schema(description = "The size of the attachment file in bytes")
+  val filesize: Int,
+  @field:Schema(description = "The filename of attachment file")
+  val filename: String,
 )
