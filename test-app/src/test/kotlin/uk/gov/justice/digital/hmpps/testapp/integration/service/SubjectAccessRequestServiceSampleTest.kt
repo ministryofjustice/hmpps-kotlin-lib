a95ed6e23ecfea6441d7a72c9de1c55cea8a7dfd
indyv-moj <inderjit.virdi@justice.gov.uk>
2026-04-29 09:39:37 +0100

    HAAR_5181: Allow for headers to be returned for SAR attachment data (#119)

--- test-app/src/test/kotlin/uk/gov/justice/digital/hmpps/testapp/integration/service/SubjectAccessRequestServiceSampleTest.kt
+++ test-app/src/test/kotlin/uk/gov/justice/digital/hmpps/testapp/integration/service/SubjectAccessRequestServiceSampleTest.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.testapp.integration.service
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.testapp.service.SubjectAccessRequestService
+import uk.gov.justice.hmpps.kotlin.sar.Attachment
+import uk.gov.justice.hmpps.kotlin.sar.AttachmentHeader
 
 class SubjectAccessRequestServiceSampleTest {
   private val service = SubjectAccessRequestService()
@@ -20,5 +22,27 @@ class SubjectAccessRequestServiceSampleTest {
 
     assertThat(restrictedPatient).extracting("prisonerNumber").isEqualTo("A12345")
     assertThat(restrictedPatient).extracting("commentText").isEqualTo("some useful comment")
+  }
+
+  @Test
+  fun `returns attachment data if found`() {
+    val attachments = service.getPrisonContentFor("A12345", null, null)?.attachments
+
+    assertThat(attachments).singleElement().isEqualTo(
+      Attachment(
+        attachmentNumber = 1,
+        name = "Attachment Image",
+        contentType = "image/gif",
+        url = "http://url/image.gif",
+        filesize = 1234,
+        filename = "image.gif",
+        headers = listOf(
+          AttachmentHeader(
+            name = "X-Header",
+            value = "header-value",
+          ),
+        ),
+      ),
+    )
   }
 }
