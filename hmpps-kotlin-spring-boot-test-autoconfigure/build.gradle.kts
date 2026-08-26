f0e31a27621ada1e4c059693c71a3cfa09a7f145
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-08-26 15:32:39 +0100

    SDIT-4176 Use latest versions (#128)

--- hmpps-kotlin-spring-boot-test-autoconfigure/build.gradle.kts
+++ hmpps-kotlin-spring-boot-test-autoconfigure/build.gradle.kts
@@ -2,8 +2,8 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "2.4.0"
-  kotlin("plugin.spring") version "2.4.0"
+  kotlin("jvm") version "2.4.10"
+  kotlin("plugin.spring") version "2.4.10"
   id("maven-publish")
   id("signing")
   id("com.adarshr.test-logger") version "4.0.0"
@@ -11,7 +11,7 @@ plugins {
   id("se.patrikerdes.use-latest-versions") version "0.2.19"
   id("io.spring.dependency-management") version "1.1.7"
   id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
-  id("org.springframework.boot") version "4.1.0"
+  id("org.springframework.boot") version "4.1.1"
 }
 
 dependencies {
